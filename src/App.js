37d962ffcdfc099c521666f109d247fb6b194d20
081dikeh <98462814+081dikeh@users.noreply.github.com>
2022-10-31 17:46:11 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from './Home';
+import {Route, Link} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
